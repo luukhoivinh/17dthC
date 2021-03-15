f519312b175598a6272566822d0bb427aedae7ee
KhoiVinh <71391115+luukhoivinh@users.noreply.github.com>
2021-03-15 16:15:18 +0700

    form dang nhap

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
     }
 }
